b06860358e15d6085656ff0ffbb9bd4cf15ab72a
keshridivya <dkeshari094@gmail.com>
2022-05-04 12:14:43 +0530

    c

--- services.php
+++ services.php
@@ -67,11 +67,11 @@ session_start();
 			
 				$count=mysqli_num_rows($sql);
 				$count1=$count/3;
-				for($col = 0; $col < $count1; $col++) {
+				for($col = 0; $col < $count; $col++) {
 			while($arr=mysqli_fetch_assoc($sql)){ 
 				$email_array[] = $arr['name'];
 				
-				for($row = 0; $row < sizeof($email_array); $row++) {
+				for($row = 0; $row < 3; $row++) {
 echo "<div class='col-lg-4 col-sm-6 mb-30'>";
 							if($row%2 == 0) {
 								echo "
