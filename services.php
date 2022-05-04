6e217b4ac23c136868fb1958d51473c6c0b9f54b
keshridivya <dkeshari094@gmail.com>
2022-05-04 12:15:25 +0530

    c

--- services.php
+++ services.php
@@ -67,7 +67,7 @@ session_start();
 			
 				$count=mysqli_num_rows($sql);
 				$count1=$count/3;
-				for($col = 0; $col < $count; $col++) {
+				for($col = 0; $col < $count1; $col++) {
 			while($arr=mysqli_fetch_assoc($sql)){ 
 				$email_array[] = $arr['name'];
 				
