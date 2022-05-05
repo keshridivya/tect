92db9a07cdb3b20c058510f6bab81b49497e3f48
keshridivya <dkeshari094@gmail.com>
2022-05-05 10:36:39 +0530

    c

--- admin/pages/forms/festival.php
+++ admin/pages/forms/festival.php
@@ -174,12 +174,7 @@ if (mysqli_num_rows($doctors)>0){
         </div>
         <!-- content-wrapper ends -->
         <!-- partial:../../partials/_footer.html -->
-        <footer class="footer">
-          <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash.</span>
-            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2021. All rights reserved.</span>
-          </div>
-        </footer>
+        <?php include("footer.ioc.php") ?>
         <!-- partial -->
       </div>
       <!-- main-panel ends -->
