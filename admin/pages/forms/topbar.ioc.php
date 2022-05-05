52749ece8e921443de3148c9cf8086b29add580c
keshridivya <dkeshari094@gmail.com>
2022-05-05 12:14:07 +0530

    c

--- admin/pages/forms/topbar.ioc.php
+++ admin/pages/forms/topbar.ioc.php
@@ -161,7 +161,7 @@ if(!isset($_SESSION['email'])){
               <div class="dropdown-header text-center">
                 <img class="img-md rounded-circle" src="../../images/faces/face8.jpg" alt="Profile image">
                 <p class="mb-1 mt-3 font-weight-semibold"><?php echo $name ?></p>
-                <p class="fw-light text-muted mb-0">allenmoreno@gmail.com</p>
+                <p class="fw-light text-muted mb-0"><?php  echo $email ?></p>
               </div>
               <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i> My Profile <span class="badge badge-pill badge-danger">1</span></a>
               <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-message-text-outline text-primary me-2"></i> Messages</a>
