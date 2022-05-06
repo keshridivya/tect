3375a4091cec1cd4da29f2b61979724a7f9ba95d
keshridivya <dkeshari094@gmail.com>
2022-05-06 15:42:57 +0530

    c

--- ch.php
+++ ch.php
@@ -138,7 +138,7 @@
 <body>
     <div class="container">
         <h2>Our  Partners</h2>
-         <section class="customer-logos slider">
+         <div class="customer-logos slider">
             <div class="slide"><img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"></div>
             <div class="slide"><img src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg"></div>
             <div class="slide"><img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"></div>
@@ -147,7 +147,7 @@
             <div class="slide"><img src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg"></div>
             <div class="slide"><img src="https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg"></div>
             <div class="slide"><img src="https://image.freepik.com/free-vector/retro-label-on-rustic-background_82147503374.jpg"></div>
-         </section>
+</div>
       </div>
 
       <script>
