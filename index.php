c62a24470b3120d3ad15935b2dfc59c458391991
keshridivya <dkeshari094@gmail.com>
2022-05-06 14:48:50 +0530

    c

--- index.php
+++ index.php
@@ -31,6 +31,7 @@ session_start();
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
 	<!--head and body in include file-->
 
 
