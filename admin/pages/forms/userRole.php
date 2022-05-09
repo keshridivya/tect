7288b5b2a75fb61c639c2ed8b05b3a9d877eb995
keshridivya <dkeshari094@gmail.com>
2022-05-09 14:44:17 +0530

    c

--- admin/pages/forms/userRole.php
+++ admin/pages/forms/userRole.php
@@ -149,7 +149,7 @@ function checkDelete(){
 
 <script>
   $('#custom7').on('change', function(){
-   this.value = this.checked ? 'enable' : 'disable';
+   this.value = this.checked ? 'disable' : 'enable';
    alert(this.value);
 }).change();
 </script>
