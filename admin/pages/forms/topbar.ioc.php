925b5f2ca3050e7959ac56762759bbd1e179b295
keshridivya <dkeshari094@gmail.com>
2022-05-10 15:08:17 +0530

    c

--- admin/pages/forms/topbar.ioc.php
+++ admin/pages/forms/topbar.ioc.php
@@ -184,7 +184,7 @@ $name=$_SESSION['name'];
                 <form method="post" id="form" class="form" enctype="multipart/form-data">
                   <div class="round">
       
-        <input class="image" name="file_upload" id="image" type="file" accept=".jpg, .jpeg, .png"/>
+        <input class="" name="file_upload" id="image" type="file" accept=".jpg, .jpeg, .png"/>
         <!--<input type="submit" name="submit" value="Upload" class="btn btn-primary image"/>-->
         <i class="mdi mdi-camera upload-button"></i>
        </div>
