101b41843989d9a0db67193e02795618c4197355
keshridivya <dkeshari094@gmail.com>
2022-05-11 15:53:38 +0530

    c

--- admin/include/configure.php
+++ admin/include/configure.php
@@ -1,7 +1,7 @@
 <?php
-$conn = mysqli_connect("localhost","u188140722_tectignis", "Admin@123", "u188140722_tectignis");
+$conn = mysqli_connect("sql476.main-hosting.eu","u188140722_tectignis", "Admin@123", "u188140722_tectignis");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
   }
- //sql476.main-hosting.eu
+ 
 ?>
