52e742ba367c444b05f49f50dd6cc477e9adf31f
keshridivya <dkeshari094@gmail.com>
2022-05-16 15:15:31 +0530

    c

--- admin/pages/forms/userRole.php
+++ admin/pages/forms/userRole.php
@@ -13,6 +13,9 @@ if(isset($_POST['upload'])){
         $user_permission=$_POST['user_permission'][$key];
     }
     $sql=mysqli_query($conn,"INSERT INTO `permission_role`(`roles`, `sidebar_id`, `status`) VALUES ('$role','$id','$user_permission')");
+    if($sql==1){
+      echo "<script>alert('Successfully Inserted');</script>";
+    }
   }
 
 ?>
