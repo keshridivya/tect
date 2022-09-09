84acac5a64334db97fe667d347af8c88fb5502ab
aniketnangare23 <aniketnangare23@gmail.com>
2022-09-09 12:02:34 +0530

    removed some links

--- index.php
+++ index.php
@@ -29,8 +29,8 @@ session_start();
     <!-- <link rel="stylesheet" href="assets/css/magnific-popup.css"> -->
     <link rel="stylesheet" href="assets/css/normalize.css"> 
     <!-- <link rel="stylesheet" href="style.css"> -->
-    <link rel="stylesheet" href="assets/css/responsive.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" />
+    <!-- <link rel="stylesheet" href="assets/css/responsive.css"> -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" /> -->
     <!-- <link rel="stylesheet" href="assets\css\reset.min.css"> -->
     <link rel="stylesheet" href="style.css" />
     <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"> -->
@@ -143,7 +143,36 @@ a {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-     
+html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline
+}
+
+article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section {
+    display: block
+}
+
+body {
+    line-height: 1
+}
+
+ol,ul {
+    list-style: none
+}
+
+table {
+    border-collapse: collapse;
+    border-spacing: 0
+}
+@media (min-width: 1200px) {
+    .container {
+        max-width: 1240px;
+    }
+}
     </style>
     <style>
         .btnReadMore {
