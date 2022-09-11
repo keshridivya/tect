20d7ce1c575fe605cb83e1df58aed70fb65b61c8
aniketnangare23 <aniketnangare23@gmail.com>
2022-09-11 23:16:56 +0530

    change

--- index.php
+++ index.php
@@ -24,14 +24,14 @@ session_start();
 	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
 	<!-- <link rel="stylesheet" href="assets/css/owl.theme.default.min.css"> -->
 	<link rel="stylesheet" href="assets/css/animate.css">
-	<!-- <link rel="stylesheet" href="assets/css/typed.css"> -->
+	<link rel="stylesheet" href="assets/css/typed.css">
 	<!-- <link rel="stylesheet" href="assets/css/magnific-popup.css"> -->
 	<link rel="stylesheet" href="assets/css/normalize.css">
 	<link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="assets/css/responsive.css">
 		
 	<!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" > -->
-	<!-- <link rel="stylesheet" href="assets\css\fontawesome.css"> -->
+	<link rel="stylesheet" href="assets\css\fontawesome.css">
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 
 	<style type="text/css">
